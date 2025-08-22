ffae5089145a32150313041f9e898504b1e4a731
Kara van Kessel <10148659+vankessel@users.noreply.github.com>
2025-08-22 01:08:47 -0700

    Camera player tracking

--- scripts/PlayerCamera.cs
+++ scripts/PlayerCamera.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using Godot;
 using StarWreck.scripts.input;
 
@@ -6,9 +7,18 @@ namespace StarWreck.scripts;
 public partial class PlayerCamera : Camera2D
 {
     [Export] private Player _player;
-    [Export(PropertyHint.Range, "0,3.1415926536")] private float _rotationSpeed = Mathf.Pi * 0.5f;
-    [Export(PropertyHint.Range, "0,4,or_greater")] private float _zoomSpeed = 2f;
-    [Export(PropertyHint.Range, "0,1,or_greater")] private float _zoomMouseWheelSensitivity = 0.05f;
+
+    [Export(PropertyHint.Range, "0,3.1415926536")]
+    private float _rotationSpeed = Mathf.Pi * 0.5f;
+
+    [Export(PropertyHint.Range, "0,4,or_greater")]
+    private float _zoomSpeed = 2f;
+
+    [Export(PropertyHint.Range, "0,1,or_greater")]
+    private float _zoomMouseWheelSensitivity = 0.05f;
+
+    [Export(PropertyHint.Range, "0,1,or_greater")]
+    private float _playerPositionTrackingHalfLife = 0.125f;
 
     public override void _Ready()
     {
@@ -39,6 +49,12 @@ public partial class PlayerCamera : Camera2D
 
         float dt = (float)delta;
 
+        HandleInput(dt);
+        Track(_player, dt);
+    }
+
+    private void HandleInput(float dt)
+    {
         float rotationInput = Input.GetAxis(Action.RotateRight, Action.RotateLeft);
         float zoomInput = Input.GetAxis(Action.ZoomOut, Action.ZoomIn);
 
@@ -47,5 +63,19 @@ public partial class PlayerCamera : Camera2D
         Rotation += radians;
 
         Zoom *= Mathf.Max(0f, 1f + zoomInput * _zoomSpeed * dt);
+    }
+
+    private void Track(Node2D node2D, float dt)
+    {
+        GlobalPosition = ExponentialMovingAverage(GlobalPosition, node2D.GlobalPosition, _playerPositionTrackingHalfLife, dt);
+    }
+
+    /// <param name="halfLife">Equal to meanLifetime*(-ln(1/2))</param>
+    [SuppressMessage("ReSharper", "InvalidXmlDocComment")]
+    private static Vector2 ExponentialMovingAverage(Vector2 current, Vector2 target, float halfLife, float dt)
+    {
+        const float lnHalf = -0.693147180559945309417232f;
+        float factor = Mathf.Exp(dt * lnHalf / halfLife);
+        return current * factor + target * (1f - factor);
     }
 }
