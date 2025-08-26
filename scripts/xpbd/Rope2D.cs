c4f1232a51863cbcd8ceebb58ed9a2493cbf09fc
Kara van Kessel <10148659+vankessel@users.noreply.github.com>
2025-08-25 18:23:09 -0700

    Remove unused class

--- scripts/xpbd/Rope2D.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-namespace StarWreck.scripts.xpbd;
-
-public partial class Rope2D : ParticlePhysicsSystem2D
-{
-    [Export(PropertyHint.Range, "0,64,or_greater")]
-    private float _length;
-}
