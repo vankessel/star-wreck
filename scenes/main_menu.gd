7c3e72497a16ab8e5a11d3369e01dfb37030dceb
PANDA-NR200P\azure <ashwong1994@gmail.com>
2025-08-31 23:23:59 -0700

    Connected main menu buttons to game

---
+++ scenes/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_credits_pressed() -> void:
+	print("Credits pressed")
+
+func _on_exit_pressed() -> void: 
+	get_tree().quit()
