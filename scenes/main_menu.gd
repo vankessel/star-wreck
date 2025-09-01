af17d0236ed35d2aed64d09b540a150bf11d02b7
PANDA-NR200P\azure <ashwong1994@gmail.com>
2025-08-31 23:31:10 -0700

    Added Credits button

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -6,7 +6,7 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_credits_pressed() -> void:
-	print("Credits pressed")
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
 func _on_exit_pressed() -> void: 
 	get_tree().quit()
