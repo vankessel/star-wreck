57d3fc7bc631b6be281e204db8ca9732ac4b58d3
Kara van Kessel <10148659+vankessel@users.noreply.github.com>
2025-09-05 22:01:10 -0700

    PauseManager class

---
+++ scripts/PauseManager.cs
@@ -0,0 +1,34 @@
+using Godot;
+using StarWreck.scripts.input;
+
+namespace StarWreck.scripts;
+
+public partial class PauseManager : Node
+{
+    public static PauseManager Instance;
+    public static ulong UnpausedPhysicsTicks { get; private set; } = 0ul;
+    public static float UnpausedSeconds => UnpausedPhysicsTicks / (float)Engine.PhysicsTicksPerSecond;
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        if (Instance != null) GD.PushError("More than one PauseManager!");
+        Instance = this;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        if (@event.IsActionPressed(Action.Pause))
+        {
+            GetTree().Paused = GetTree().IsPaused();
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        UnpausedPhysicsTicks += 1ul;
+    }
+}
--- scripts/input/Action.cs
+++ scripts/input/Action.cs
@@ -4,12 +4,13 @@ namespace StarWreck.scripts.input;
 
 public static class Action
 {
-    public static StringName Up = new("Up");
-    public static StringName Down = new("Down");
-    public static StringName Left = new("Left");
-    public static StringName Right = new("Right");
-    public static StringName RotateLeft = new("RotateLeft");
-    public static StringName RotateRight = new("RotateRight");
-    public static StringName ZoomIn = new("ZoomIn");
-    public static StringName ZoomOut = new("ZoomOut");
+    public static readonly StringName Up = new("Up");
+    public static readonly StringName Down = new("Down");
+    public static readonly StringName Left = new("Left");
+    public static readonly StringName Right = new("Right");
+    public static readonly StringName RotateLeft = new("RotateLeft");
+    public static readonly StringName RotateRight = new("RotateRight");
+    public static readonly StringName ZoomIn = new("ZoomIn");
+    public static readonly StringName ZoomOut = new("ZoomOut");
+    public static readonly StringName Pause = new("Pause");
 }
