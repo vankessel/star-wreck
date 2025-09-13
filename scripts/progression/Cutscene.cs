7644982a4f805897441873ac05e37f46b5b012a9
Kara van Kessel <10148659+vankessel@users.noreply.github.com>
2025-09-13 12:51:07 -0700

    Change tabs back to spaces

--- scripts/progression/Cutscene.cs
+++ scripts/progression/Cutscene.cs
@@ -6,112 +6,121 @@ namespace StarWreck.scripts.progression;
 [GlobalClass]
 public partial class Cutscene : Node
 {
-	[Export] private bool _pause = true;
-	[Export] private PackedScene _scene;
-	[Export(PropertyHint.File, "*.dialogue")] private Resource _dialogue;
+    [Export] private bool _pause = true;
+    [Export] private PackedScene _scene;
 
-	[Signal] public delegate void CutsceneStartedEventHandler();
-	[Signal] public delegate void CutsceneFinishedEventHandler();
-	[Signal] public delegate void CutsceneSequenceStartedEventHandler();
-	[Signal] public delegate void CutsceneSequenceFinishedEventHandler();
+    [Export(PropertyHint.File, "*.dialogue")]
+    private Resource _dialogue;
 
-	private Node _instancedScene;
-	private Cutscene _previousCutsceneInSequence;
-	private Cutscene _nextCutsceneInSequence;
+    [Signal]
+    public delegate void CutsceneStartedEventHandler();
 
-	public override void _Ready()
-	{
-		base._Ready();
+    [Signal]
+    public delegate void CutsceneFinishedEventHandler();
 
-		_nextCutsceneInSequence = GetChildOrNull<Cutscene>(0);
-		if (_nextCutsceneInSequence != null)
-		{
-			_nextCutsceneInSequence._previousCutsceneInSequence = this;
-		}
+    [Signal]
+    public delegate void CutsceneSequenceStartedEventHandler();
 
-		DialogueManager.DialogueStarted += DialogueStarted;
-		DialogueManager.DialogueEnded += DialogueEnded;
-		CutsceneFinished += OnCutsceneFinished;
-	}
+    [Signal]
+    public delegate void CutsceneSequenceFinishedEventHandler();
 
-	public override void _ExitTree()
-	{
-		base._ExitTree();
-		DialogueManager.DialogueStarted -= DialogueStarted;
-		DialogueManager.DialogueEnded -= DialogueEnded;
-		CutsceneFinished -= OnCutsceneFinished;
-	}
+    private Node _instancedScene;
+    private Cutscene _previousCutsceneInSequence;
+    private Cutscene _nextCutsceneInSequence;
 
-	private void DialogueStarted(Resource dialogueResource)
-	{
-		if (dialogueResource != _dialogue) return;
-		EmitSignal(SignalName.CutsceneStarted);
-	}
+    public override void _Ready()
+    {
+        base._Ready();
 
-	private void DialogueEnded(Resource dialogueResource)
-	{
-		if (dialogueResource != _dialogue) return;
+        _nextCutsceneInSequence = GetChildOrNull<Cutscene>(0);
+        if (_nextCutsceneInSequence != null)
+        {
+            _nextCutsceneInSequence._previousCutsceneInSequence = this;
+        }
 
-		_instancedScene?.QueueFree();
+        DialogueManager.DialogueStarted += DialogueStarted;
+        DialogueManager.DialogueEnded += DialogueEnded;
+        CutsceneFinished += OnCutsceneFinished;
+    }
 
-		GetTree().Paused = false;
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        DialogueManager.DialogueStarted -= DialogueStarted;
+        DialogueManager.DialogueEnded -= DialogueEnded;
+        CutsceneFinished -= OnCutsceneFinished;
+    }
 
-		EmitSignal(SignalName.CutsceneFinished);
-	}
+    private void DialogueStarted(Resource dialogueResource)
+    {
+        if (dialogueResource != _dialogue) return;
+        EmitSignal(SignalName.CutsceneStarted);
+    }
 
-	private void OnCutsceneStarted()
-	{
-		if (_previousCutsceneInSequence != null) return;
-		Cutscene currentCutscene = this;
-		while (currentCutscene != null)
-		{
-			currentCutscene.EmitSignal(SignalName.CutsceneSequenceStarted);
-			currentCutscene = currentCutscene._nextCutsceneInSequence;
-		}
-	}
+    private void DialogueEnded(Resource dialogueResource)
+    {
+        if (dialogueResource != _dialogue) return;
 
-	private void OnCutsceneFinished()
-	{
-		if (_nextCutsceneInSequence != null)
-		{
-			_nextCutsceneInSequence.Play();
-		}
-		else
-		{
-			Cutscene currentCutscene = this;
-			while (currentCutscene != null)
-			{
-				currentCutscene.EmitSignal(SignalName.CutsceneSequenceFinished);
-				currentCutscene = currentCutscene._previousCutsceneInSequence;
-			}
-		}
-	}
+        _instancedScene?.QueueFree();
 
-	public void Play()
-	{
-		if (_dialogue != null)
-		{
-			if (_scene != null)
-			{
-				_instancedScene = _scene.Instantiate();
-				_instancedScene.ProcessMode = ProcessModeEnum.Always;
-				AddChild(_instancedScene);
-				_instancedScene.Owner = GetTree().Root;
+        GetTree().Paused = false;
 
-				GetTree().Paused = true;
-			}
-			else
-			{
-				GetTree().Paused = _pause;
-			}
+        EmitSignal(SignalName.CutsceneFinished);
+    }
 
-			Node node = DialogueManager.ShowDialogueBalloon(_dialogue);
-			node.ProcessMode = ProcessModeEnum.Always;
-		}
-		else
-		{
-			EmitSignal(SignalName.CutsceneFinished);
-			GD.PushWarning($"No dialogue set for {Name}");
-		}
-	}
+    private void OnCutsceneStarted()
+    {
+        if (_previousCutsceneInSequence != null) return;
+        Cutscene currentCutscene = this;
+        while (currentCutscene != null)
+        {
+            currentCutscene.EmitSignal(SignalName.CutsceneSequenceStarted);
+            currentCutscene = currentCutscene._nextCutsceneInSequence;
+        }
+    }
+
+    private void OnCutsceneFinished()
+    {
+        if (_nextCutsceneInSequence != null)
+        {
+            _nextCutsceneInSequence.Play();
+        }
+        else
+        {
+            Cutscene currentCutscene = this;
+            while (currentCutscene != null)
+            {
+                currentCutscene.EmitSignal(SignalName.CutsceneSequenceFinished);
+                currentCutscene = currentCutscene._previousCutsceneInSequence;
+            }
+        }
+    }
+
+    public void Play()
+    {
+        if (_dialogue != null)
+        {
+            if (_scene != null)
+            {
+                _instancedScene = _scene.Instantiate();
+                _instancedScene.ProcessMode = ProcessModeEnum.Always;
+                AddChild(_instancedScene);
+                _instancedScene.Owner = GetTree().Root;
+
+                GetTree().Paused = true;
+            }
+            else
+            {
+                GetTree().Paused = _pause;
+            }
+
+            Node node = DialogueManager.ShowDialogueBalloon(_dialogue);
+            node.ProcessMode = ProcessModeEnum.Always;
+        }
+        else
+        {
+            EmitSignal(SignalName.CutsceneFinished);
+            GD.PushWarning($"No dialogue set for {Name}");
+        }
+    }
 }
