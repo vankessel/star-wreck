4af01c8b1cd69ea719ff1c60a4d161ee73227758
PANDA-NR200P\azure <ashwong1994@gmail.com>
2025-09-14 00:38:00 -0700

    Fixed some dialogues to mention why the wrecking ball

--- scenes/main_menu/MainMenu.cs
+++ scenes/main_menu/MainMenu.cs
@@ -4,40 +4,40 @@ namespace StarWreck.scenes.main_menu;
 
 public partial class MainMenu : Node
 {
-    [Export(PropertyHint.File, "*.tscn")] private string _startScene;
-    [Export(PropertyHint.File, "*.tscn")] private string _creditsScene;
-    [Export] private Button _startButton;
-    [Export] private Button _creditsButton;
-    [Export] private Button _exitButton;
+	[Export(PropertyHint.File, "*.tscn")] private string _startScene;
+	[Export(PropertyHint.File, "*.tscn")] private string _creditsScene;
+	[Export] private Button _startButton;
+	[Export] private Button _creditsButton;
+	[Export] private Button _exitButton;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        _startButton.Pressed += StartButtonOnPressed;
-        _creditsButton.Pressed += CreditsButtonOnPressed;
-        _exitButton.Pressed += ExitButtonOnPressed;
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		_startButton.Pressed += StartButtonOnPressed;
+		_creditsButton.Pressed += CreditsButtonOnPressed;
+		_exitButton.Pressed += ExitButtonOnPressed;
+	}
 
-    public override void _ExitTree()
-    {
-        base._ExitTree();
-        _startButton.Pressed -= StartButtonOnPressed;
-        _creditsButton.Pressed -= CreditsButtonOnPressed;
-        _exitButton.Pressed -= ExitButtonOnPressed;
-    }
+	public override void _ExitTree()
+	{
+		base._ExitTree();
+		_startButton.Pressed -= StartButtonOnPressed;
+		_creditsButton.Pressed -= CreditsButtonOnPressed;
+		_exitButton.Pressed -= ExitButtonOnPressed;
+	}
 
-    private void StartButtonOnPressed()
-    {
-        GetTree().ChangeSceneToFile(_startScene);
-    }
+	private void StartButtonOnPressed()
+	{
+		GetTree().ChangeSceneToFile(_startScene);
+	}
 
-    private void CreditsButtonOnPressed()
-    {
-        GetTree().ChangeSceneToFile(_creditsScene);
-    }
+	private void CreditsButtonOnPressed()
+	{
+		GetTree().ChangeSceneToFile(_creditsScene);
+	}
 
-    private void ExitButtonOnPressed()
-    {
-        GetTree().Quit();
-    }
+	private void ExitButtonOnPressed()
+	{
+		GetTree().Quit();
+	}
 }
